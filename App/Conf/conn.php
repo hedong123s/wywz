c9406940b7ba468e81cb87e3df2689dfa724a711
hedong <dong.he01@billionsfinance.cn>
2016-12-06 12:48:04 +0800

    con

--- App/Conf/conn.php
+++ App/Conf/conn.php
@@ -1,6 +1,6 @@
 <?php 
 
-$type='';
+$type='2';
 if(empty($type)){
 	return array(
 		//数据库配置信息
@@ -17,10 +17,10 @@ if(empty($type)){
 	return array(
 		//数据库配置信息
 		'DB_TYPE'   => 'mysql', // 数据库类型
-		'DB_HOST'   => 'localhost', // 服务器地址
-		'DB_NAME'   => 'fuding', // 数据库名
-		'DB_USER'   => 'fuding', // 用户名
-		'DB_PWD'    => 'fuding1205', // 密码
+		'DB_HOST'   => '115.28.73.129', // 服务器地址
+		'DB_NAME'   => 'wywz', // 数据库名
+		'DB_USER'   => 'root', // 用户名
+		'DB_PWD'    => 'Gaodunming5', // 密码
 		'DB_PORT'   => 3306, // 端口
 		'DB_PREFIX' => '9f_', // 数据库表前缀 
 	);
